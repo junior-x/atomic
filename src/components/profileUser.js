a895b335976f86e65c92d6305a6dc81508c81119
junior-x <marcosmansur@outlook.com>
2022-02-03 07:50:27 -0800

    hello-world

---
+++ src/components/profileUser.js
@@ -0,0 +1,12 @@
+import "./../styles/userStyles.scss"
+function ProfileUser() {
+  return (
+    <div class="container">
+  <div class="grid-4">Coluna de 4</div>
+  <div class="grid-4">Coluna de 4</div>
+  <div class="grid-4">Coluna de 4</div>
+</div>
+  );
+}
+
+export default ProfileUser;
