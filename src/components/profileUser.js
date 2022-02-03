b345ec4859f8a2a04d640c4ae9f3da10a85dbfa6
junior-x <marcosmansur@outlook.com>
2022-02-03 08:35:06 -0800

    grid

--- src/components/profileUser.js
+++ src/components/profileUser.js
@@ -1,12 +1,15 @@
 import "./../styles/userStyles.scss"
 function ProfileUser() {
-  return (
-    <div class="container">
-  <div class="grid-4">Coluna de 4</div>
-  <div class="grid-4">Coluna de 4</div>
-  <div class="grid-4">Coluna de 4</div>
-</div>
-  );
+    return (
+        <div class="body">
+            <div class="container">
+                <div class="grid-1">Coluna de 4</div>
+                <div class="grid-2">Coluna de 2</div>
+                <div class="grid-4">Coluna de 4</div>
+            </div>
+        </div>
+
+    );
 }
 
 export default ProfileUser;
