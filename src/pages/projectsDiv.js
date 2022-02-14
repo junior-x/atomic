df5d90454e54b20d6402d9db15da91d06b793397
junior-x <marcosmansur@outlook.com>
2022-02-13 17:39:27 -0800

    hackinLoop

--- src/pages/projectsDiv.js
+++ src/pages/projectsDiv.js
@@ -1,4 +1,6 @@
 import React from "react";
+import LottieBadcode from "../assets/badcode/lottieBadcode";
+import LottieHackintosh from "../assets/hackintosh/LottieHackintosh";
 import LottiePacman from "../assets/pacman/lottiePacman";
 import './../App.css';
 
@@ -7,11 +9,24 @@ export default function ProjectsDiv() {
     return(
         <>
             <div className="projectsDiv">
-                <div className="projectsTitle">☂️ Projects</div>
-                <div>
-                    <div></div>
-                    <div className="projectsPacman"><LottiePacman/></div>
-                    <div></div>
+                <div className="projectsTitle">Projects</div>
+                <div className="projectsList">
+                    <div className="projectsHackintosh">
+                        <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}  onClick={() => window.open("https://hackintosh-one.vercel.app/")}>
+                            <LottieHackintosh/>
+                            <h3>HACKINTOSH</h3>
+                        </div>
+                        <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}  onClick={() => window.open("https://pac-man-site.vercel.app/")} >
+                            <LottiePacman />
+                            <h3>PAC-MAN THEME</h3>
+                        </div>
+                        <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}  onClick={() => window.open("https://bad-code.vercel.app/")}>
+                            <LottieBadcode />
+                            <h3>BAD-CODE CHAT</h3>
+                        </div>
+                    </div>
+                    {/* <div className="projectsPacman"><LottiePacman/></div>
+                    <div className="projectsBadCode"></div> */}
                 </div>
             </div>
         </>
